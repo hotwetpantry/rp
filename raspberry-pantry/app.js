f20b921db1ca09ae7d2e4a7c2aad110087c5d2e5
hotwetpantry <hotwetpantry@gmail.com>
2017-08-10 18:31:41 +0300

    added new features

--- raspberry-pantry/app.js
+++ raspberry-pantry/app.js
@@ -38,24 +38,24 @@ app.use('/girls', girls);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 var Datastore = require('nedb');
-var db = new Datastore({filename : 'girls2'});
+var db = new Datastore({ filename: 'girls2' });
 db.loadDatabase();
 app.set("girlsDb", db);
 module.exports = app;
